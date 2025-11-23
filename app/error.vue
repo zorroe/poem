efedd412d697610416e9156d3f678d1dc5ec1eb5
zorroe <1318753541@qq.com>
2025-11-23 14:06:31 +0800

    添加错误页面

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <UApp>
+    <UError :error="error" />
+  </UApp>
+</template>
---
+++ app/pages/[slug].vue
@@ -0,0 +1,4 @@
+<script setup lang="ts">
+throw createError({ statusCode: 404, statusMessage: '页面不存在', fatal: true })
+
+</script>
